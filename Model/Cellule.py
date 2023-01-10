b4681197e3b5e4eeb5779fd27cfde67bd06471c1
Ruben <ruben.dunesme@gmail.com>
2023-01-10 13:20:43 +0100

    construireCellule : tests ok

--- Model/Cellule.py
+++ Model/Cellule.py
@@ -13,7 +13,9 @@ def type_cellule(cell: dict) -> bool:
     Détermine si le paramètre est une cellule correcte ou non
 
     :param cell: objet dont on veut tester le type cellule
+    :type cell:dict
     :return: True si c'est une cellule, False sinon
+    :rtype: bool
     """
     return type(cell) == dict and const.CONTENU in cell and const.VISIBLE in cell \
         and type(cell[const.VISIBLE] == bool) and type(cell[const.CONTENU]) == int \
@@ -24,7 +26,9 @@ def isContenuCorrect(contenuCellule:int)->bool:
     """
     Détermine si le paramètre  peut représenter le contenu d’une cellule
     :param contenuCellule: entier représentant le contenu d'une cellule
+    :type contenuCellule:int
     :return: 'True' si le contenu de la cellule est un entier compris entre 0 et 8 ou égal à const.ID_MINE , 'False' sinon
+    :rtype: bool
     """
 
     return  type(contenuCellule) == int and ((0 <= contenuCellule <= 8) or contenuCellule == const.ID_MINE)
@@ -33,8 +37,12 @@ def construireCellule(contenuCellule:int=0,visibilite:bool=False)->dict:
     """
     Crée une cellule en indiquant son contenu et sa visibilité
     :param contenuCellule: entier représentant le contenu d'une cellule
+    :type contenuCellule:int
     :param visibilite: un booléen correspondant à la visibilité (initialisé par défaut à 'False')
+    :type visibilite: bool
+    :raises ValueError:
     :return: Le dictionnaire correspondant à cette cellule
+    :rtype:dict
     """
     if not isContenuCorrect(contenuCellule):
         raise ValueError(f"construireCellule : le contenu {contenuCellule} n’est pas correct ")
@@ -43,3 +51,5 @@ def construireCellule(contenuCellule:int=0,visibilite:bool=False)->dict:
         raise TypeError(f"construireCellule : le second paramètre {type(visibilite)} n’est pas un booléen")
 
     return {const.CONTENU : contenuCellule,const.VISIBLE: visibilite}
+
+
